f8dfbbbfb47c6177725c95b9f1558c8a141500ec
Pryanxhufr <165806890+Pryanxhufr@users.noreply.github.com>
2025-04-13 21:09:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import time
 import requests
 
-url = "https://apis-3kns.onrender.com/?product_id=3"
+url1 = "https://apis-3kns.onrender.com/?product_id=3"
+url2 = "https://imgbot-3y9h.onrender.com"
 
 while True:
     try:
-        response = requests.get(url)
-        print(response)
+        response1 = requests.get(url1)
+        print("URL1:", response1)
+
+        response2 = requests.get(url2)
+        print("URL2:", response2)
     except:
         pass  # Ignore all errors
-    time.sleep(40)
+    time.sleep(35)
