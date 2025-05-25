53b9b14bbdf9dac9d1c8c28dd4ef174402659a15
Pryanxhufr <165806890+Pryanxhufr@users.noreply.github.com>
2025-05-25 14:17:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import time
 import requests
 
 url1 = "https://api-fjm4.onrender.com/"
-url2 = "https://imgbot-3y9h.onrender.com"
+url2 = "https://api-ikit.onrender.com/?product_id=2"
 
 while True:
     try:
         response1 = requests.get(url1)
         print("URL1:", response1)
 
-        #response2 = requests.get(url2)
-        #print("URL2:", response2)
+        response2 = requests.get(url2)
+        print("URL2:", response2)
     except:
         pass  # Ignore all errors
     time.sleep(40)
